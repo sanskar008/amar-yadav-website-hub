3b1d9066ffb35f7469dc87b41428e17ea92436be
Sanskar Koserwal <sanskark008@gmail.com>
2025-05-27 18:25:39 +0530

    bg colour change

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,35 +1,34 @@
-
-import React from 'react';
-import Header from '../components/Header';
-import AboutSection from '../components/AboutSection';
-import InstagramFeed from '../components/InstagramFeed';
-import EventsSection from '../components/EventsSection';
-import Footer from '../components/Footer';
+import React from "react";
+import Header from "../components/Header";
+import AboutSection from "../components/AboutSection";
+import InstagramFeed from "../components/InstagramFeed";
+import EventsSection from "../components/EventsSection";
+import Footer from "../components/Footer";
 
 const Index = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-indigo-50">
+    <div className="min-h-screen bg-gradient-to-b from-[#FFB300] to-[#FF6F00]">
       <Header />
-      
+
       <main className="container mx-auto px-4 py-8">
         <div className="grid grid-cols-1 lg:grid-cols-12 gap-6">
           {/* About Section - Left */}
           <div className="lg:col-span-4">
             <AboutSection />
           </div>
-          
+
           {/* Instagram Feed - Center */}
           <div className="lg:col-span-5">
             <InstagramFeed />
           </div>
-          
+
           {/* Events Section - Right */}
           <div className="lg:col-span-3">
             <EventsSection />
           </div>
         </div>
       </main>
-      
+
       <Footer />
     </div>
   );
